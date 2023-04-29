22fb2b870d76c364dfd2d75cdafe14ff929c9d66
Juan Gabriel Guzman <juan.guzman@levadata.com>
2023-04-28 21:48:27 -0500

    Adding mongoose to integration test

--- tests/integration/database.test.ts
+++ tests/integration/database.test.ts
@@ -5,15 +5,34 @@ import mongoose from 'mongoose';
 describe('Array', function () {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', async function () {
-            const mongoServer = await MongoMemoryServer.create();
+            const mongoServer = await MongoMemoryServer.create({
+                instance: {
+                    port: 27017
+                },
+            });
             const uri = mongoServer.getUri();
-            await mongoose.connect(mongoServer.getUri(), { dbName: "verifyMASTER" });
+            await mongoose.connect(uri);
 
-            // your code here
+            const kittySchema = new mongoose.Schema({
+                name: String
+            });
+            const Kitten = mongoose.model('Kitten', kittySchema);
+            const silence = new Kitten({ name: 'Silence' });
 
+            console.log(silence.name); // 'Silence'
+            await silence.save();
+
+            const response = await Kitten.findOne({ 'name': 'Silence' }).exec();
+            console.log(response);
+            await delay(20000);
             await mongoose.disconnect();
             await mongoServer.stop();
-            expect(true).to.be.true;
+            expect(response.name).to.eq("Silence");
         });
     });
 });
+
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
